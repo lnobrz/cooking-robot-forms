6429490977ac9ffce06d703b235aad39f5201e52
Lucas Nobre <lucasnobre37@outlook.com>
2022-11-30 21:54:34 -0300

    customize tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,31 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        lightRed: "#FFEADF",
+        brightRed: "#E71E1E",
+        softWhite: "#FFFDFD",
+        grey: "#EFEFEF",
+        brightGreen: "#48bf53",
+        darkerGrey: "#D6D6D6",
+      },
+      fontFamily: {
+        display: ["BalooThambi2Bold"],
+        text: ["ArialRoundedMTBold"],
+      },
+      spacing: {
+        534: "534px",
+        565: "565px",
+        1000: "1000px",
+        5: "5%",
+        50: "50%",
+      },
+      screens: {
+        "xsm": "400px",
+        "3xl": "1808px",
+      }
+    },
   },
   plugins: [],
-}
+};
