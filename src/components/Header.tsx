954515c96421bf5603b69925aaf4734bea713aa3
Lucas Nobre <lucasnobre37@outlook.com>
2022-11-30 21:56:34 -0300

    add Header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Logo from "./ui/Logo";
+const Header = () => {
+  return (
+    <nav className="bg-softWhite">
+      <Logo />
+    </nav>
+  );
+};
+
+export default Header;
