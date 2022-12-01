6662b9ac90c1457f99f8aca90aa40aeb70eabc76
Lucas Nobre <lucasnobre37@outlook.com>
2022-11-30 21:55:34 -0300

    create Logo component

---
+++ src/components/ui/Logo.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Logo = () => {
+  return (
+    <img
+      src="/assets/images/logo.png"
+      alt="cooking robot logo"
+      className="w-20 h-20 ml-7 mt-7"
+    />
+  );
+};
+
+export default Logo;
